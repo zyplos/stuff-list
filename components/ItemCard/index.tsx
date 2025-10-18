6e1eba5327a32bf80446fcb912d44c27b2f6f6dc
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-18 15:25:38 -0500

    better card colors

--- components/ItemCard/index.tsx
+++ components/ItemCard/index.tsx
@@ -52,10 +52,8 @@ export default function ItemCard({
 
         {item.unlikelyToFind && (
           <div className={styles.infoAlert}>
-            <span className={styles.infoIcon}>ℹ</span>
-            <span>
-              You're unlikely to find this but, keep a lookout for it!
-            </span>
+            <InfoIcon className={styles.infoIcon} />
+            <span>You're unlikely to find this but keep a lookout for it!</span>
           </div>
         )}
 
@@ -63,14 +61,14 @@ export default function ItemCard({
 
         <div className={styles.buttons}>
           <button
-            className={styles.foundButton}
+            className={clsx(styles.button, styles.foundButton)}
             onClick={onFoundIt}
             type="button"
           >
             Found it!
           </button>
           <button
-            className={styles.expensiveButton}
+            className={clsx(styles.button, styles.expensiveButton)}
             onClick={onSawButExpensive}
             type="button"
           >
@@ -79,5 +77,19 @@ export default function ItemCard({
         </div>
       </div>
     </div>
+  );
+}
+
+function InfoIcon({ ...props }: React.SVGProps<SVGSVGElement>) {
+  return (
+    // biome-ignore lint/a11y/noSvgWithoutTitle: decoration
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+    </svg>
   );
 }
