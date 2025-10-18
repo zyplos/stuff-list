ab4540c3191223b312034a5eafd029f32acd73e2
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-18 10:53:25 -0500

    added style stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import "@/styles/globals.css";
+import "@/styles/_variables.css";
+import "@/styles/_globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
