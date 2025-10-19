181f3097f959d287b1e94a5a87e8a5a1cb6c8c06
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-19 09:51:51 -0500

    added kit kat item

--- internals/items.tsx
+++ internals/items.tsx
@@ -4,13 +4,15 @@ import x1b from "@/assets/strange-3ds/x1b.jpg";
 import x2 from "@/assets/strange-3ds/x2.jpg";
 import x2Back from "@/assets/strange-3ds/x2-back.jpg";
 
+import kitKats from "@/assets/kit-kats/seafood.png";
+
 export const alwaysActiveItems: Item[] = [
   {
     id: "kit-kats",
     name: "kit kats",
     description:
-      "bring back all the ones you find. don't bring ones you've seen in the US already",
-    images: [redBlue, x1b, x2, x2Back],
+      "bring back all the ones you find\ndon't bring ones you've seen in the US already",
+    images: [kitKats],
   },
 ];
 
