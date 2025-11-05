4b04708f6aea26bce712217aab951d19e7bc5322
Angel <22824872+Zyplos@users.noreply.github.com>
2025-11-05 00:18:12 -0600

    move hardcoded firebase config into env variables

--- internals/firebase/config.ts
+++ internals/firebase/config.ts
@@ -3,14 +3,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRQPyOavEapNsb2dK6Mhv3HaujT85Ui1k",
-  authDomain: "stuff-list-6a998.firebaseapp.com",
-  projectId: "stuff-list-6a998",
-  storageBucket: "stuff-list-6a998.firebasestorage.app",
-  messagingSenderId: "666835900051",
-  appId: "1:666835900051:web:9b41f1b1d54ecc90665f66",
-  databaseURL:
-    "https://stuff-list-6a998-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
